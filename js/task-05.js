dd2deba71399de8b6e1cb5b69eb64a8a6fb0916d
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-02 17:03:41 +0100

    add task-05

---
+++ js/task-05.js
@@ -0,0 +1,27 @@
+console.log('task-05');
+console.log('');
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const arrNew = [];
+
+  for (const iterator of arr) {
+    if (!Object.keys(iterator).includes(prop)) return [];
+
+    arrNew.push(iterator[prop]);
+  }
+
+  return arrNew;
+};
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
